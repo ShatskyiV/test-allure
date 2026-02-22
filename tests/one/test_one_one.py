9219a6e8f3345b95ee5beb9f0aa0aaf847e0c927
Vitalii Shatskyi <vitalii.shatskyi@seagate.com>
2026-02-22 13:43:23 +0100

    test allure

---
+++ tests/one/test_one_one.py
@@ -0,0 +1,6 @@
+def test_one_one_ok():
+    assert True
+
+def test_one_one_fail():
+    assert False
+
---
+++ tests/one/test_one_two.py
@@ -0,0 +1,5 @@
+def test_one_two_ok():
+    assert True
+
+def test_one_two_fail():
+    assert False
